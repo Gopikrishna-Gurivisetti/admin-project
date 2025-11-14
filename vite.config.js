acefc48aba7bc7debee5395b729163024c1a3f1a
Gurivisetti <gopikrishnagurivisetti098@gmail.com>
2025-11-14 09:43:31 +0530

    added base path for versel deployement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import process from "process";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_ADMIN_PATH || "/admin-project",
+  });
+};
